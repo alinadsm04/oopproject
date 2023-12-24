c13c6af618bd734fea4a6186be1febdd91868c67
zhulduzz <123058905+zhulduzz@users.noreply.github.com>
2023-12-24 22:52:01 +0600

    Update CitationsCompartor.java

--- Researcher/CitationsCompartor.java
+++ Researcher/CitationsCompartor.java
@@ -1,11 +1,22 @@
-package teamproject;
+package projectSystem;
 
 import java.util.Comparator;
 
+
 public class CitationsCompartor implements Comparator<ResearchPaper>{
 
-	public int compare(teamproject.ResearchPaper a, teamproject.ResearchPaper b) {
-		return a.getCitations().compareTo(b.getCitations());
-	}
+	public int compare(ResearchPaper a, ResearchPaper b) {
+		int[] firstCitations = a.getCitations();
+		int[] secondCitations = b.getCitations();
+		
+		for (int i = 0; i < Math.min(firstCitations.length, secondCitations.length); i++) {
+            int comparisonResult = Integer.compare(firstCitations[i], secondCitations[i]);
+            if (comparisonResult != 0) {
+                return comparisonResult;
+            }
+        }
+		
+	return Integer.compare(firstCitations.length, secondCitations.length);
 	
+}
 }
