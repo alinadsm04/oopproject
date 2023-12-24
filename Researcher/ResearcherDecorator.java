1852f1674cb646fbae0d1143e5a2c96b12292a47
zhulduzz <123058905+zhulduzz@users.noreply.github.com>
2023-12-24 23:01:14 +0600

    Add files via upload

---
+++ Researcher/ResearcherDecorator.java
@@ -0,0 +1,30 @@
+package teamproject;
+
+public abstract class ResearcherDecorator implements Researcher{
+	protected final Researcher decoratedResearcher;
+	
+	public ResearcherDecorator(Researcher r) {
+		this.decoratedResearcher = r;
+	}
+	
+	public int calculateHIndex() {
+		return decoratedResearcher.calculateHIndex();
+	}
+	
+	public ResearchPaper printPapers() {
+		return decoratedResearcher.printPapers();
+	}
+	
+	public boolean joinResearchProject() {
+		return decoratedResearcher.joinResearchProject();
+	}
+	
+	public ResearchProject doResearchProject() {
+		return decoratedResearcher.doReseacrhProject();
+	}
+	
+	public ResearchPaper writeResearchPaper() {
+		return decoratedResearcher.writeResearchPaper();
+	}
+
+}
---
+++ Researcher/Test.java
@@ -0,0 +1,18 @@
+package teamproject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line = in.readLine();
+        System.out.println("Выбеите число: \n1 \n2");
+        if (line == "2") {
+        	System.out.println(line);
+        }
+	}
+
+}
